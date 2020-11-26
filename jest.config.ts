ddeccf4f7d265062c0ac47972e399a2f13350d3b
Pedro Meneghel <pedromeneghel@gmail.com>
2020-11-26 16:38:26 -0300

    Teste de criação de appointment

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -31,7 +34,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  // coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -81,7 +84,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
