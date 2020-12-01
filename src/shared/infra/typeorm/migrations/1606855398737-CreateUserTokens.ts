074ad69aad0ad209904dfc63e4fc527ade16fc52
Pedro Meneghel <pedromeneghel@gmail.com>
2020-12-01 17:49:49 -0300

    Iniciada a criação da funcionalidade de reset de senha

---
+++ src/shared/infra/typeorm/migrations/1606855398737-CreateUserTokens.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTokens1606855398737 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
